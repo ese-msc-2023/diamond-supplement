09c77c7201619054f91ff8f60833fb4900bcecfd
rolotumazi <jfvs41@gmailcom>
2024-02-08 10:59:39 +0000

    edited path to be generic

--- Diamond.sh
+++ Diamond.sh
@@ -4,7 +4,7 @@ myvar=$1
 chrlen=${#myvar}
 
 if [ "$chrlen" -ge 4 ]; then
-    diamond -s /<ABSOLUTE_PATH_TO_INSTALLATION_FOLDER>/ICFERST/schemas/multiphase.rng ./$myvar
+    diamond -s ${HOME}/diamond-supplement/multiphase.rng ./$myvar
 else
-    diamond -s /<ABSOLUTE_PATH_TO_INSTALLATION_FOLDER>/ICFERST/schemas/multiphase.rng ./*.mpml
+    diamond -s ${HOME}/diamond-supplement/multiphase.rng ./*.mpml
 fi
